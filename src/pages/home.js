4fe49768909a38a2a69473c7ea63c385c60ec4b8
castellanoscc <castellanosc0520@gmail.com>
2024-01-30 13:31:18 -0500

    Contact Page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 function Home() {
@@ -13,6 +13,7 @@ function Home() {
             <Hero />
             <AboutMe />
             <Projects />
+            <Contact />
             <Footer />
         </>
     );
