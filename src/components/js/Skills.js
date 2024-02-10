8899fb2db444ef9bdabef9630bcac3af35e2fe77
castellanoscc <castellanosc0520@gmail.com>
2024-02-10 02:01:43 -0500

    Warning fix

--- src/components/js/Skills.js
+++ src/components/js/Skills.js
@@ -1,49 +1,48 @@
 import "../css/Skills.css";
 
-const skillsIcons = [
-    {
-        img: "https://skillicons.dev/icons?i=html",
-        id: 1,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=css",
-        id: 2,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=js",
-        id: 3,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=bootstrap",
-        id: 4,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=react",
-        id: 5,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=sass",
-        id: 6,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=django",
-        id: 7,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=jquery",
-        id: 8,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=mysql",
-        id: 9,
-    },
-    {
-        img: "https://skillicons.dev/icons?i=nodejs",
-        id: 10,
-    },
-];
-
 function Skills() {
+    const skillsIcons = [
+        {
+            img: "https://skillicons.dev/icons?i=html",
+            id: 1,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=css",
+            id: 2,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=js",
+            id: 3,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=bootstrap",
+            id: 4,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=react",
+            id: 5,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=sass",
+            id: 6,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=django",
+            id: 7,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=jquery",
+            id: 8,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=mysql",
+            id: 9,
+        },
+        {
+            img: "https://skillicons.dev/icons?i=nodejs",
+            id: 10,
+        },
+    ];
     return (
         <>
             <section id="skills" className="skills-list">
